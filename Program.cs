a0775a566d3cf5c77712efc5d8208b60360dbcfd
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-03-25 16:09:38 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Cebolinha trocar o L por R (Frase Cebolinha = frase com Dislalia)
+
+String Frase;
+String FraseCebolinha;
+
+Console.Write("Digite uma frase: ");
+Frase = Console.ReadLine()!;
+Console.WriteLine();
+
+FraseCebolinha = Frase
+
+.Replace ("r", "l")
+.Replace("R","L");
+
+Console.WriteLine($"Frase com Dislalia: {FraseCebolinha}");
